8b8753a94b695ed41f15f6544d93ebe640848de8
kennarttech <kennartech@gmail.com>
2023-01-25 13:20:34 -0600

    changes made to progress.py and Readme.md

--- app/progress_process.py
+++ app/progress_process.py
@@ -1,8 +1,9 @@
 import os
 from tkinter import *
 import customtkinter
-import login
-from tkinter.ttk import Progressbar
+from tkinter import ttk
+
+
 
 
 """window.geometry(1366x768)"""
@@ -10,12 +11,13 @@ customtkinter.set_appearance_mode('dark')
 
 
 window = customtkinter.CTk()
-window.geometry('530x400+416+165')
-window.title('Loading Progress')
-img = PhotoImage(file='/home/kenneth/Desktop/desktop_ap/app/icons/AA.png')
-window.tk.call('wm', 'iconphoto', window._w, img)
-window.minsize(500, 400)
 window.resizable(0, 0)
+window.minsize(500, 400)
+window.title('Loading Progress')
+window.geometry('530x400+416+165')
+img = PhotoImage(file='app/icons/AA.png')
+window.tk.call('wm', 'iconphoto', window._w, img)
+
 
 
 window.columnconfigure(0, weight=1)
@@ -24,44 +26,51 @@ window.rowconfigure(0, weight=1)
 window.rowconfigure(1, weight=0)
 
 
-label = customtkinter.CTkLabel(master=window, text = 'Welcome to Kennart Tech', 
+
+label = customtkinter.CTkLabel(master=window, text = 'Welcome To DS Enterprise', 
                                 font=('Times', 22), )
 label.grid(row=0, column=0, ipady=30, ipadx=30, sticky=N)
 
 
 progress_label = customtkinter.CTkLabel(master = window, text = 'Please Wait....', 
-                                font = ('Times', 20))
+                                font = ('Times', 23))
 progress_label.grid(row=1, column=0,  sticky=NSEW)
 
 
 
 progress = customtkinter.CTkProgressBar(master = window, width = 480, 
-                                    height = 15, mode='determinate', 
-                                    orientation='horizontal')
+                                    height = 18, mode='determinate', 
+                                    orientation='horizontal',
+                                    border_width=1, progress_color='#16CF13')
 progress.grid(row=2, column=0, padx=20, pady=20, sticky=EW)
-progress.start()
+progress.set(0.0)
 
 
 
-def userlogin():
+
+def user_login():
     window.withdraw()
-    os.system('python', 'login.py')
+    os.system("python app/login.py")
     window.destroy()
 
 
-i = 0
-def progressload():
-    global i
-    if i <= 10:
-        txt = 'Please Wait.... ' + (str(10 * i) + '%')
-        progress_label.configure(text=txt)
-        progress_label.after(1000, progressload)
-        progress['value'] = 10 * i
-        i += 1
-    else:
-        userlogin()
 
-progressload()
+counter = 0
+def progressload(value):
+    """This function is responsible for loading the page"""
+    progress.set(value)
+    global counter
+    if counter <= 10:
+        test = 'Please Wait.... ' + (str(10 * counter) + '%')
+        progress_label.configure(text=test)
+        window.after(1000, progressload, value + 1/10)
+        counter += 1
+    else:
+        user_login()
+
+progressload(0)
+
+
 
 
 
