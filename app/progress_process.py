98ad8a92e9fa0ebb25499ad613dd77e0ab8bf600
kennarttech <kennartech@gmail.com>
2023-01-25 16:49:48 -0600

    changes made to progress.py and Readme.md

--- app/progress_process.py
+++ app/progress_process.py
@@ -1,6 +1,6 @@
 import os
-from tkinter import *
 import customtkinter
+from tkinter import *
 from tkinter import ttk
 
 
@@ -33,13 +33,13 @@ label.grid(row=0, column=0, ipady=30, ipadx=30, sticky=N)
 
 
 progress_label = customtkinter.CTkLabel(master = window, text = 'Please Wait....', 
-                                font = ('Times', 23))
+                                font = ('Roboto', 18))
 progress_label.grid(row=1, column=0,  sticky=NSEW)
 
 
 progress = customtkinter.CTkProgressBar(master = window, width = 480, 
                                     height = 18, mode='determinate', 
-                                    orientation='horizontal',
+                                    orientation='horizontal', determinate_speed= 2,
                                     border_width=1, progress_color='#16CF13')
 progress.grid(row=2, column=0, padx=20, pady=20, sticky=EW)
 progress.set(0.0)
@@ -48,6 +48,7 @@ progress.set(0.0)
 
 
 def user_login():
+    """This function defines user login page after ProgressBar finish loading"""
     window.withdraw()
     os.system("python app/login.py")
     window.destroy()
@@ -57,6 +58,8 @@ counter = 0
 def progressload(value):
     """This function is responsible for loading the page"""
     progress.set(value)
+    result = progress.get()
+    print(result)
     global counter
     if counter <= 10:
         test = 'Please Wait.... ' + (str(10 * counter) + '%')
