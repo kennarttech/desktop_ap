e6a670ff6ce3f15f89a6bb32c8dbfa1d010c89e2
kennarttech <kennartech@gmail.com>
2023-01-26 00:36:39 -0600

    New files has been added to app

--- app/homepage.py
+++ app/homepage.py
@@ -3,8 +3,9 @@ from tkinter import *
 import customtkinter
 
 
+
+
 customtkinter.set_appearance_mode('dark')
-"""window.geometry(1366x768)"""
 
 root = customtkinter.CTk()
 root.geometry('800x500+300+130')
